92475aa90da55eefab1199ecbdd808b7dbce61b4
kynglx <kaylaangeline22@gmail.com>
2026-06-09 22:00:42 +0000

    Add Prometheus and Grafana monitoringalso a few fixes

--- app/app.py
+++ app/app.py
@@ -1,35 +1,98 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import Response
+
 from prometheus_client import Counter
+from prometheus_client import Histogram
+from prometheus_client import Gauge
 from prometheus_client import generate_latest
 from prometheus_client import CONTENT_TYPE_LATEST
-from fastapi.responses import Response
+
+import mlflow
+import mlflow.pyfunc
+import pandas as pd
 import os
+import time
+import socket
 
 app = FastAPI()
+
+# =========================
+# PROMETHEUS METRICS
+# =========================
 
 REQUEST_COUNT = Counter(
     "movie_api_requests_total",
     "Total API Requests"
 )
 
+REQUEST_LATENCY = Histogram(
+    "movie_api_latency_seconds",
+    "API Latency"
+)
+
+LAST_PREDICTION = Gauge(
+    "movie_last_prediction",
+    "Latest Prediction Value"
+)
+
+# =========================
+# LOAD MODEL FROM MLFLOW
+# =========================
+
+mlflow.set_tracking_uri(
+    os.getenv(
+        "MLFLOW_TRACKING_URI",
+        "http://mlflow-server:5000"
+    )
+)
+
+model = mlflow.pyfunc.load_model(
+    "models:/movie-popularity-model/latest"
+)
+
+# =========================
+# INPUT SCHEMA
+# =========================
+
+class MovieInput(BaseModel):
+    vote_average: float
+    vote_count: int
+    release_year: int
+    is_recent: int
+    is_popular: int
+    genre_count: int
+    is_english: int
+    is_adult: int
+
+# =========================
+# ENDPOINTS
+# =========================
+
 @app.get("/")
 def home():
+
     REQUEST_COUNT.inc()
 
     return {
         "message": "Movie Popularity Prediction API"
     }
 
+
 @app.get("/health")
 def health():
+
     REQUEST_COUNT.inc()
 
     return {
-        "status": "healthy"
+        "status": "healthy",
+        "container": socket.gethostname()
     }
+
 
 @app.get("/mlflow")
 def mlflow_connection():
+
     REQUEST_COUNT.inc()
 
     return {
@@ -38,8 +101,46 @@ def mlflow_connection():
         )
     }
 
+
+@app.post("/predict")
+def predict(data: MovieInput):
+
+    REQUEST_COUNT.inc()
+
+    start_time = time.time()
+
+    input_df = pd.DataFrame([{
+        "vote_average": data.vote_average,
+        "vote_count": data.vote_count,
+        "release_year": data.release_year,
+        "is_recent": data.is_recent,
+        "is_popular": data.is_popular,
+        "genre_count": data.genre_count,
+        "is_english": data.is_english,
+        "is_adult": data.is_adult
+    }])
+
+    prediction = model.predict(input_df)
+
+    latency = time.time() - start_time
+
+    REQUEST_LATENCY.observe(latency)
+
+    LAST_PREDICTION.set(
+        float(prediction[0])
+    )
+
+    return {
+        "predicted_popularity": float(
+            prediction[0]
+        ),
+        "latency_seconds": latency
+    }
+
+
 @app.get("/metrics")
 def metrics():
+
     return Response(
         generate_latest(),
         media_type=CONTENT_TYPE_LATEST
--- src/training/train.py
+++ src/training/train.py
@@ -5,8 +5,7 @@ import mlflow.sklearn
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import (mean_squared_error, r2_score)
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 
 mlflow.set_tracking_uri("file:./mlruns")
 
@@ -14,32 +13,38 @@ mlflow.set_tracking_uri("file:./mlruns")
 df = pd.read_csv("data/processed/dataset.csv")
 
 # fitur & target
-X = df[[
-    "vote_average",
-    "vote_count",
-    "release_year",
-    "is_recent",
-    "is_popular",
-    "genre_count",
-    "is_english",
-    "is_adult"
-]]
+X = df[
+    [
+        "vote_average",
+        "vote_count",
+        "release_year",
+        "is_recent",
+        "is_popular",
+        "genre_count",
+        "is_english",
+        "is_adult"
+    ]
+]
 
 y = df["popularity_log"]
 
 # split data
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
 )
 
-RMSE_THRESHOLD = 110
+RMSE_THRESHOLD = 1.0
 
 
 def train_model(n_estimators, max_depth):
 
-    with mlflow.start_run():
+    with mlflow.start_run(
+        run_name=f"RF_{n_estimators}_{max_depth}"
+    ) as run:
 
-        # model
         model = RandomForestRegressor(
             n_estimators=n_estimators,
             max_depth=max_depth,
@@ -48,58 +53,88 @@ def train_model(n_estimators, max_depth):
 
         model.fit(X_train, y_train)
 
-        # prediksi
         y_pred = model.predict(X_test)
 
-        # metric
         r2 = r2_score(y_test, y_pred)
         rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
-        # logging
-        mlflow.log_param("n_estimators", n_estimators)
-        mlflow.log_param("max_depth", max_depth)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-
-        print(
-            f"Run selesai | n_estimators={n_estimators}, max_depth={max_depth}, RMSE={rmse:.2f}, R2={r2:.4f}"
+        mlflow.log_param(
+            "n_estimators",
+            n_estimators
         )
 
-        return model, rmse
+        mlflow.log_param(
+            "max_depth",
+            max_depth
+        )
+
+        mlflow.log_metric(
+            "rmse",
+            rmse
+        )
+
+        mlflow.log_metric(
+            "r2",
+            r2
+        )
+
+        print(
+            f"Run selesai | "
+            f"n_estimators={n_estimators}, "
+            f"max_depth={max_depth}, "
+            f"RMSE={rmse:.4f}, "
+            f"R2={r2:.4f}"
+        )
+
+        return model, rmse, run.info.run_id
 
 
 if __name__ == "__main__":
 
     experiments = [
+        (50, 10),
+        (100, 20),
         (200, 30)
     ]
 
     best_model = None
     best_rmse = float("inf")
+    best_run_id = None
+    best_params = None
 
     for n_estimators, max_depth in experiments:
 
-        model, rmse = train_model(
+        model, rmse, run_id = train_model(
             n_estimators,
             max_depth
         )
 
         if rmse < best_rmse:
+
             best_rmse = rmse
             best_model = model
+            best_run_id = run_id
+
+            best_params = {
+                "n_estimators": n_estimators,
+                "max_depth": max_depth
+            }
 
     print(f"Best RMSE: {best_rmse}")
+    print(f"Best Parameters: {best_params}")
 
-    # validation threshold
     if best_rmse <= RMSE_THRESHOLD:
 
         print("✅ Model lolos validasi")
 
-        mlflow.sklearn.log_model(
-            best_model,
-            "model",
-            registered_model_name="movie-popularity-model"
-        )
+        # buka lagi run terbaik
+        with mlflow.start_run(run_id=best_run_id):
+
+            mlflow.sklearn.log_model(
+                best_model,
+                "model",
+                registered_model_name="movie-popularity-model"
+            )
 
     else:
         raise Exception(
